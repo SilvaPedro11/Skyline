d6f76232ccda639d9b4e8425087eb9ebb243771c
SilvaPedro11 <pedoca.360@gmail.com>
2022-08-25 22:00:26 -0300

    App

---
+++ app/src/main/java/br/etecia/padaria/Componente.java
@@ -0,0 +1,14 @@
+package br.etecia.padaria;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Componente extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.componente_layout);
+    }
+}
